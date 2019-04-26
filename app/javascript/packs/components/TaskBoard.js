db854b6776f6a603c8d4470d13332d3ab919dc4e
greybutton <greybutton@yandex.com>
2019-04-26 11:08:43 +0800

    drag task

--- app/javascript/packs/components/TaskBoard.js
+++ app/javascript/packs/components/TaskBoard.js
@@ -99,6 +99,15 @@ export default class TaskBoard extends React.Component {
     };
   }
 
+  handleDragEnd(cardId, sourceLaneId, targetLaneId) {
+    fetch("PUT", window.Routes.api_v1_task_path(cardId, { format: "json" }), {
+      task: { state: targetLaneId }
+    }).then(() => {
+      this.loadLine(sourceLaneId);
+      this.loadLine(targetLaneId);
+    });
+  }
+
   render() {
     return (
       <div>
@@ -108,6 +117,9 @@ export default class TaskBoard extends React.Component {
           onLaneScroll={this.onLaneScroll}
           customLaneHeader={<LaneHeader />}
           cardsMeta={this.state}
+          draggable
+          laneDraggable={false}
+          handleDragEnd={this.handleDragEnd}
         />
       </div>
     );
